6453a2d798db681b99f62ef6f5fa36176aa3ee0a
Deni06068 <Deni06068@gmail.com>
2022-03-31 12:14:12 +0300

    MirrorWords changed

--- SoftUni/ProgramingFundamentals/src/JavaFundamentalsExercise/PracticeTests/MirrorWords.java
+++ SoftUni/ProgramingFundamentals/src/JavaFundamentalsExercise/PracticeTests/MirrorWords.java
@@ -14,17 +14,27 @@ public class MirrorWords {
         Matcher matcher = pattern.matcher(line);
         List<String> words = new ArrayList<>();
         Map<String, String> wordGroups = new LinkedHashMap<>();
+        int countPairs = 0;
 
-        while (matcher.find()){
+        while (matcher.find()) {
             words.add(matcher.group(2));
         }
         for (int i = 0; i < words.size() - 1; i++) {
-            StringBuilder sb = new StringBuilder(words.get(i+1));
-            if (words.get(i).equals(sb.reverse().toString())){
-                wordGroups.put(words.get(i), sb.toString());
+            StringBuilder sb = new StringBuilder(words.get(i + 1));
+            if (words.get(i).equalsIgnoreCase((sb.reverse().toString()))) {
+                countPairs++;
                 i++;
             }
         }
+        for (int i = 0; i < words.size() - 1; i++) {
+            StringBuilder sb = new StringBuilder(words.get(i + 1));
+            if (words.get(i).equals(sb.reverse().toString())) {
+                wordGroups.put(words.get(i), sb.reverse().toString());
+                i++;
+            }
+        }
+        words.forEach(System.out::println);
+        System.out.println(countPairs);
         String mapAsString = wordGroups.keySet().stream()
                 .map(key -> key + " <=> " + wordGroups.get(key))
                 .collect(Collectors.joining(", "));
